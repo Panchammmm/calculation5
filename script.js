31024e6ab151ede9a7ee517cefec8e09ae9b3119
Panchammmm <sardarpancham510510@gmail.com>
2024-02-26 22:01:59 +0530

    new functions and items added

--- script.js
+++ script.js
@@ -20,7 +20,23 @@ function clearField() {
   resultField.value = '';
 }
 
+// Function to handle backspace
+function backspace() {
+  resultField.value = resultField.value.slice(0, -1);
+}
+
 // Custom function to evaluate expression without using eval
 function evaluateExpression(expression) {
   return Function('"use strict";return (' + expression + ')')();
+}
+
+// Function to handle 'e', 'log', and '%'
+function calculateSpecial(operation) {
+  if (operation === 'e') {
+    resultField.value += Math.E;
+  } else if (operation === 'log') {
+    resultField.value = Math.log(resultField.value);
+  } else if (operation === '%') {
+    resultField.value = eval(resultField.value) / 100;
+  }
 }
